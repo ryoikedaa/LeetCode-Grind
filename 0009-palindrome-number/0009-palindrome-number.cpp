e9f3d8b255d1212a5dbb02f4585dabfc51debb33
Ryo Ikeda <ryoikeda@umich.edu>
2025-06-01 23:37:39 -0400

    Time: 2 ms (52.8%), Space: 8.6 MB (43.3%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+        if (reverse == xcopy) {
+            return true;
+        }
+        return false;
+        }
+};
