f32ba66c5d221102506457abb6d3eac0a13ca59e
Ryo Ikeda <ryoikeda@umich.edu>
2025-07-29 15:57:43 -0400

    Time: 12 ms (39.07%), Space: 11.6 MB (96.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,20 +9,19 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-
         ListNode* fast = head;
         ListNode* slow = head;
-       
+
         while (fast != nullptr && fast->next != nullptr) {
-             slow = slow->next;
-        fast = fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
 
-
-        
-        if (slow == fast) {
-            return true;
-        }
+            if (slow == fast) {
+                return true;
+            }
         }
         return false;
     }
+
 };
+
