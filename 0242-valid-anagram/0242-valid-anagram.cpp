aafc329a6ae72c0b1d06b83deed866d113c2944e
Ryo Ikeda <ryoikeda@umich.edu>
2025-09-28 14:07:15 -0400

    Time: 6 ms (25.17%), Space: 9.6 MB (74.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (t.size() != s.size()) {
+        if (s.length() != t.length()) {
             return false;
         }
+
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
