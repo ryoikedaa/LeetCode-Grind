b4e4223ee3ef29099d4c0f4ca32c651595917bc7
Ryo Ikeda <ryoikeda@umich.edu>
2025-09-29 19:30:55 -0400

    Time: 16 ms (61.61%), Space: 26 MB (48.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
+        vector<vector<string>> output;
 
-        for (string s: strs) {
-            string partofstring = s;
-            sort(partofstring.begin(), partofstring.end());
-            mp[partofstring].push_back(s);
+        for (auto& s: strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
         }
-        vector<vector<string>> anagrams;
 
-        for (auto p: mp) {
-            anagrams.push_back(p.second);
+        for (auto p : mp) {
+            output.push_back(p.second);
         }
-        return anagrams;
+        return output;
+        
     }
 };
