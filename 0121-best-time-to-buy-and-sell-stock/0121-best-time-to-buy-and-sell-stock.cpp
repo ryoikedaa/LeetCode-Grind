3110fcb648902c86e3b6ade0b536b80b69ad3f6a
Ryo Ikeda <ryoikeda@umich.edu>
2025-12-30 19:01:15 -0500

    Time: 0 ms (100%), Space: 97.3 MB (59.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buyPrice = prices[0];
-        int profit = 0;
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
         for (int i = 0; i < prices.size(); i++) {
-            if (buyPrice > prices[i]) {
-                buyPrice = prices[i];
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
             }
-            profit = max(profit, prices[i] - buyPrice);
-        }
-        return profit;
+            else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        } 
+        return maxProfit;
     }
 };
